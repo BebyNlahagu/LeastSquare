47699db8e23c190330bb11e394c28e081a442ba2
BebyNlahagu <bebynlahagu20@gmail.com>
2023-07-30 00:03:45 +0700

    Least Square

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -102,7 +102,7 @@
                 <div class="modal-body">Pilih logout Untuk Mengakhiri Sesi</div>
                 <div class="modal-footer">
                     <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="{{ rouet('login') }}">Logout</a>
+                    <a class="btn btn-primary" href="{{ route('login') }}">Logout</a>
                 </div>
             </div>
         </div>
