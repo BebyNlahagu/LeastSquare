70031be0ac4105913ca4504f0d0d047f2ff4a86e
BebyNlahagu <bebynlahagu20@gmail.com>
2023-08-09 22:23:16 +0700

    Least Square

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,4 +1,6 @@
+<div style="text-align: center"><strong>Prediksi Penjualan Baterai Mobil pada Toko Rabin Baterai</strong></div>
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
 
     <!-- Sidebar Toggle (Topbar) -->
     <a id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
@@ -53,7 +55,7 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{{ auth()->user()->name }}</span>
+                <span class="mr-2 d-none d-lg-inline text-gray-600 bold"><strong>{{ auth()->user()->name }}</strong></span>
                 <img class="img-profile rounded-circle"
                     src="{{asset('auth/img/undraw_profile.svg')}}">
             </a>
